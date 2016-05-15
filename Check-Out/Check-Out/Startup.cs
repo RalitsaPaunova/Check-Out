006d892dbb4fb4cdbf3cd05233418e81807c1e15
Rdebugger <ralica.paunova6@gmail.com>
2016-05-15 16:57:11 +0300

    First

---
+++ Check-Out/Check-Out/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Check_Out.Startup))]
+namespace Check_Out
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
